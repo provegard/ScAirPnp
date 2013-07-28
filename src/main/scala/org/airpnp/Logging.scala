bf0037b2a573138c2c05e8dc6e1b369c9309fd59
Per Rovegard <per@rovegard.se>
2013-07-28 20:44:56 +0200

    Fixed output problem in Logging.

--- src/main/scala/org/airpnp/Logging.scala
+++ src/main/scala/org/airpnp/Logging.scala
@@ -6,10 +6,10 @@ import org.slf4j.Logger
 trait Logging {
     protected val logger: Logger = LoggerFactory.getLogger(getClass)
     
-    protected def debug(msg: String, os: AnyRef*) = logger.debug(msg, os)
-    protected def trace(msg: String, os: AnyRef*) = logger.trace(msg, os)
-    protected def info(msg: String, os: AnyRef*) = logger.info(msg, os)
-    protected def warn(msg: String, os: AnyRef*) = logger.warn(msg, os)
-    protected def error(msg: String, os: AnyRef*) = logger.error(msg, os)
+    protected def debug(msg: String, os: AnyRef*) = logger.debug(msg, os: _*)
+    protected def trace(msg: String, os: AnyRef*) = logger.trace(msg, os: _*)
+    protected def info(msg: String, os: AnyRef*) = logger.info(msg, os: _*)
+    protected def warn(msg: String, os: AnyRef*) = logger.warn(msg, os: _*)
+    protected def error(msg: String, os: AnyRef*) = logger.error(msg, os: _*)
     protected def error(msg: String, t: Throwable) = logger.error(msg, t)
 }
