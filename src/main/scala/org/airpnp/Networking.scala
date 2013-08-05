fbdf742ba95f69ea3e6fcce36000aeb89d3b2b0f
Per Rovegard <per@rovegard.se>
2013-08-06 00:45:39 +0200

    Added Networking.creatDownloader.

--- src/main/scala/org/airpnp/Networking.scala
+++ src/main/scala/org/airpnp/Networking.scala
@@ -5,9 +5,27 @@ import net.pms.PMS
 import java.net.InetAddress
 import net.pms.network.NetworkConfiguration
 import net.pms.configuration.PmsConfiguration
+import scala.xml.Node
+import java.net.URLConnection
+import java.net.HttpURLConnection
+import java.net.URL
+import scala.xml.XML
 
 object Networking {
-   
+
+  def createDownloader(): String => Node = {
+    loc =>
+      {
+        val url = new URL(loc)
+        val is = url.openStream
+        try {
+          XML.load(is)
+        } finally {
+          is.close
+        }
+      }
+  }
+
   def getInetAddress(): InetAddress = {
     val configuration = PMS.getConfiguration()
     val hostname = configuration.getServerHostname
@@ -21,13 +39,13 @@ object Networking {
       InetAddress.getByName(hostname)
     }
   }
-  
+
   private def getAddressFromNetworkInterface(networkInterfaceName: String): InetAddress = {
     var ia = isEmpty(networkInterfaceName) match {
       case true => NetworkConfiguration.getInstance.getDefaultNetworkInterfaceAddress()
       case false => NetworkConfiguration.getInstance.getAddressForNetworkInterfaceName(networkInterfaceName)
     }
-    
+
     if (ia != null) ia.getAddr else InetAddress.getLocalHost
   }
 
