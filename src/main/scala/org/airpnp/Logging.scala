e12c5f3ed72fd4e3bf527545481c5367ac002416
Per Rovegard <per@rovegard.se>
2013-08-12 10:45:13 +0200

    Logging fix.

--- src/main/scala/org/airpnp/Logging.scala
+++ src/main/scala/org/airpnp/Logging.scala
@@ -6,10 +6,10 @@ import org.slf4j.Logger
 trait Logging {
     protected val logger: Logger = LoggerFactory.getLogger(getClass)
     
-    protected def debug(msg: String, os: Any*) = logger.debug(msg, os.map(_.toString))
-    protected def trace(msg: String, os: Any*) = logger.trace(msg, os.map(_.toString))
-    protected def info(msg: String, os: Any*) = logger.info(msg, os.map(_.toString))
-    protected def warn(msg: String, os: Any*) = logger.warn(msg, os.map(_.toString))
-    protected def error(msg: String, os: Any*) = logger.error(msg, os.map(_.toString))
+    protected def debug(msg: String, os: Any*) = logger.debug(msg, os.map(_.toString): _*)
+    protected def trace(msg: String, os: Any*) = logger.trace(msg, os.map(_.toString): _*)
+    protected def info(msg: String, os: Any*) = logger.info(msg, os.map(_.toString): _*)
+    protected def warn(msg: String, os: Any*) = logger.warn(msg, os.map(_.toString): _*)
+    protected def error(msg: String, os: Any*) = logger.error(msg, os.map(_.toString): _*)
     protected def error(msg: String, t: Throwable) = logger.error(msg, t)
 }
