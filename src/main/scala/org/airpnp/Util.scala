dcbd29b3fa1fdbc433929c4bc8395031b421cd95
Per Rovegard <per@rovegard.se>
2013-08-13 17:54:00 +0200

    Removed unused Util code.

--- src/main/scala/org/airpnp/Util.scala
+++ src/main/scala/org/airpnp/Util.scala
@@ -25,11 +25,6 @@ import org.xml.sax.InputSource;
 
 object Util {
 
-  //    private static final DocumentBuilderFactory factory = DocumentBuilderFactory
-  //            .newInstance();
-  //    private static final XPathFactory xpFactory = XPathFactory.newInstance();
-  //
-
   /**
    * Split a USN into a UDN and a device or service type.
    * <p>
@@ -44,54 +39,7 @@ object Util {
     val parts = usn.split("::")
     if (parts.length == 2) (parts(0), parts(1)) else (parts(0), "")
   }
-  //
-  //    public static Document loadXMLFromStream(InputStream xmlStream)
-  //            throws Exception {
-  //        // TODO: Wrap exceptions and throw some own XML-ish exception type
-  //        DocumentBuilder builder = factory.newDocumentBuilder();
-  //        InputSource is = new InputSource(xmlStream);
-  //        return builder.parse(is);
-  //    }
-  //
-  //    public static XPathExpression compileXPath(String expr) {
-  //        XPath xp = xpFactory.newXPath();
-  //        try {
-  //            return xp.compile(expr);
-  //        } catch (XPathExpressionException e) {
-  //            return null; // Causes NPE at runtime
-  //        }
-  //    }
-  //
-  //    public static Node findNode(Node start, XPathExpression xp) {
-  //        try {
-  //            return (Node) xp.evaluate(start, XPathConstants.NODE);
-  //        } catch (XPathExpressionException e) {
-  //            throw new RuntimeException(e);
-  //        }
-  //    }
-  //
-  //    public static List<Node> findNodes(Node start, XPathExpression xp) {
-  //        try {
-  //            NodeList list = (NodeList) xp.evaluate(start,
-  //                    XPathConstants.NODESET);
-  //            List<Node> result = new ArrayList<Node>();
-  //            for (int i = 0; i < list.getLength(); i++) {
-  //                result.add(list.item(i));
-  //            }
-  //            return result;
-  //        } catch (XPathExpressionException e) {
-  //            throw new RuntimeException(e);
-  //        }
-  //    }
-  //
-  //    public static String findText(Node start, XPathExpression xp) {
-  //        try {
-  //            return (String) xp.evaluate(start, XPathConstants.STRING);
-  //        } catch (XPathExpressionException e) {
-  //            throw new RuntimeException(e);
-  //        }
-  //    }
-  //
+
   def areServiceTypesCompatible(required: String, actual: String): Boolean = {
     if (required == actual)
       return true
