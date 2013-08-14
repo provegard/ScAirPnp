85a6577a42bb9448d7b4284b980b451d4b70a1ec
Per Rovegard <per@rovegard.se>
2013-08-14 19:50:32 +0200

    Added pretty-printed XML to SoapError.

--- src/main/scala/org/airpnp/upnp/SoapError.scala
+++ src/main/scala/org/airpnp/upnp/SoapError.scala
@@ -2,16 +2,23 @@ package org.airpnp.upnp
 
 import java.io.InputStream
 import scala.xml.XML
+import java.io.StringWriter
+import scala.xml.PrettyPrinter
 
 object SoapError {
   def parse(is: InputStream) = {
     //TODO: error handling
-    val err = XML.load(is) \\ "UPnPError"
+    val root = XML.load(is)
+    
+    val pp = new PrettyPrinter(80, 2)
+    val xml = pp.format(root)
+    
+    val err = root \\ "UPnPError"
     val errorCode = Integer.parseInt((err \ "errorCode").text)
     val errorDesc = (err \ "errorDescription").text
-    new SoapError(errorCode, errorDesc)
+    new SoapError(errorCode, errorDesc, xml)
   }
 }
 
-class SoapError private(val code: Int, val description: String) extends RuntimeException(description) {
+class SoapError private(val code: Int, val description: String, val xml: String) extends RuntimeException(code + " " + description) {
 }
